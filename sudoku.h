5a3262235c355a4fe824108d283992301cb58f51
häckfleischbällchen04 <ludwle01@thu.de>
2026-07-04 22:08:06 +0200

    Update sudoku.h
    Anpassungen miteinander in Einklang bringen

--- sudoku.h
+++ sudoku.h
@@ -5,9 +5,9 @@
 
 int main();
 int solve(int row, int column, int sudoku[9][9]); 
-int validate_cell(int row, int column, int number, int sudoku[9][9]);
+int validate_cell(int row, int column, int number, int sudoku[9][9]); // einzelnes Feld checken
 int** read();
-int validate_matrix(int** matrix);
+int validate(int** matrix); // ganzes Sudoku checken
 void output(int** matrix);
 void printMatrix(int** matrix);
 
